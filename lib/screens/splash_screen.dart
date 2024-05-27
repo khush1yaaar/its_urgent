deafb42a14ac366f7fa64c8175fe5117fe2ae14b
Harkirat Singh <sandhukirat23@gmail.com>
2024-05-27 19:47:44 +1000

    splash screen ui edit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:its_urgent/screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +24,17 @@ class SplashScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/cropped_image.png', width: 250,),
-          const SizedBox(width: double.infinity, height: 50,),
+          Image.asset(
+            'assets/cropped_image.png',
+            width: 250,
+          ),
+          const SizedBox(
+            width: double.infinity,
+            height: 50,
+          ),
           Text("It's Urgent !", style: Theme.of(context).textTheme.displayLarge)
         ],
       ),
-      
     );
   }
 }
