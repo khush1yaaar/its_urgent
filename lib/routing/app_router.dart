06821e0abfe5a3abf76830109d584a3d05e22bc3
Harkirat Singh <sandhukirat23@gmail.com>
2024-05-28 13:00:23 +1000

    add riverpod with goRouter

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:its_urgent/screens/home_screen.dart';
 import 'package:its_urgent/screens/splash_screen.dart';
@@ -7,19 +8,21 @@ enum AppRoutes {
   homeScreen,
 }
 
-final goRouter = GoRouter(
-  initialLocation: '/',
-  debugLogDiagnostics: true,
-  routes: [
-    GoRoute(
-      path: '/',
-      name: AppRoutes.splashScreen.name,
-      builder: (context, state) => const SplashScreen(),
-    ),
-    GoRoute(
-      path: '/homeScreen',
-      name: AppRoutes.homeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-  ],
-);
+final goRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        name: AppRoutes.splashScreen.name,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: '/homeScreen',
+        name: AppRoutes.homeScreen.name,
+        builder: (context, state) => const HomeScreen(),
+      ),
+    ],
+  );
+});
