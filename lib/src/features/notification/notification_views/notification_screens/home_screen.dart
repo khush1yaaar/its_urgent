74e0a5a3e09ce77f969411c68cfefbe3f5994c55
sandhukirat23 <sandhukirat23@gmail.com>
2024-06-22 16:03:27 +1000

    all emulators setup done

--- lib/src/features/notification/notification_views/notification_screens/home_screen.dart
+++ lib/src/features/notification/notification_views/notification_screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:its_urgent/src/commons/common_providers/firebase_auth_provider.dart';
