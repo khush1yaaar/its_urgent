b9be2c4c60e6ec2606fdb7e47296713249db33a9
Harkirat Singh <sandhukirat23@gmail.com>
2024-10-23 19:29:20 +1100

    fix: background notification

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,9 @@ Future<void> setupEmulators() async {
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   log('Got a message whilst in the background!');
   log('Message data: ${message.data}');
